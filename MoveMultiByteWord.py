ca62a2a9f78ec0cdd420e8e20cfacd7949b1dff3
dat <mmaga@dstribe.net>
2014-11-02 21:29:50 +0900

    Issue #2 よく使う文字列で移動できないものに対応 MoveMultiByteWord.py 改行コード・全角記号・全角アルファベット・全角数字に対応, パターンを辞書型に変更

--- MoveMultiByteWord.py
+++ MoveMultiByteWord.py
@@ -1,20 +1,33 @@
-import sublime, sublime_plugin
+import sublime_plugin
 import re
 
+
 def regularPattern():
-    reCjk         = '[\u3400-\u9FFF\uF900-\uFAFF]+'
-    reHiragana    = '[\u3040-\u309F]+'
-    reKatakana    = '[\u30A0-\u30FA\u30FC-\u30FE]+'
-    reCjkSymbol   = '[\u3000-\u3020]+'
-    reMultiSymbol = '[\u005C\u00A2\u00A3\u00A7\u00A8\u00AC\u00B0\u00B1\u00B4\u00B6\u00D7\u00F7\u2010\u2015\u2016\u2018\u2019\u201C\u201D\u2020\u2021\u2025\u2026\u2030\u2032\u2033\u203B\u2103]+'
-    reLatin       = '[\u0030-\u0039\u0041-\u005A\u0061-\u007A\u0020\u005F]+'
-    reLatinSymbol = '[\u0021-\u002F\u003A-\u0040\u005B-\u005E\u0060\u00A5\u007B-\u007D\u203E]+'
-    reCompile = re.compile(u''+reCjk+'|'+reHiragana+'|'+reKatakana+'|[\u30FB]+|[\u30FF]+|'+reCjkSymbol+'|'+reLatin+'|'+reLatinSymbol+'|'+reMultiSymbol)
+    separate = '|'
+    re_pattern = {
+        'cjk': '[\u3400-\u9FFF\uF900-\uFAFF]+',
+        'hiragana': '[\u3040-\u309F]+',
+        'katakana': '[\u30A0-\u30FA\u30FC-\u30FE]+',
+        'cjkSymbol': '[\u3000-\u3020]+',
+        'fullDigit': '[\uFF10-\uFF19]+',
+        'fullLatin': '[\uFF21-\uFF3A\uFF41-\uFF5A]+',
+        'halfCjkPunctuation': '[\uFF61-\uFF65]',
+        'halfKatakana': '[\uFF66-\uFF9F]',
+        'fullSymbol': '[\uFF01-\uFF0F\uFF1A-\uFF20\uFF3B-\uFF40\uFF5B-\uFF60\uFFE0-\uFFE6\u005C\u00A2\u00A3\u00A7\u00A8\u00AC\u00B0\u00B1\u00B4\u00B6\u00D7\u00F7\u2010\u2015\u2016\u2018\u2019\u201C\u201D\u2020\u2021\u2025\u2026\u2030\u2032\u2033\u203B\u2103]+',
+        'halfSymbol': '[\uFFE8-\uFFEE]',
+        'latin': '[\u0030-\u0039\u0041-\u005A\u0061-\u007A\u0020\u005F]+',
+        'latinSymbol': '[\u0021-\u002F\u003A-\u0040\u005B-\u005E\u0060\u00A5\u007B-\u007D\u203E]+',
+        'controlCharacters': '[\u000D\u000A-\u000C]'
+    }
+    pattern = separate.join(re_pattern.values())
+    reCompile = re.compile(u''+pattern)
     return reCompile
+
 
 def wordParse(text):
     reCompile = regularPattern()
     return reCompile.findall(text)
+
 
 class MoveMultiByteWordCommand(sublime_plugin.TextCommand):
     def run(self, edit, forward=False):
@@ -30,10 +43,10 @@ class MoveMultiByteWordCommand(sublime_plugin.TextCommand):
             self.view.run_command("move", {"by": "characters", "forward": courseA})
             return True
         setIndex = -1
-        courseB   = True
-        if courseA == True:
+        courseB = True
+        if courseA is True:
             setIndex = 0
-            courseB   = False
+            courseB = False
         length = len(results[setIndex])
         self.view.run_command("move", {"by": "characters", "forward": courseB})
         while length > 0:
@@ -41,7 +54,9 @@ class MoveMultiByteWordCommand(sublime_plugin.TextCommand):
             length -= 1
         return results
 
+
 class MoveSelMultiByteWordCommand(sublime_plugin.TextCommand):
+
     def run(self, edit, forward=False):
         for region in self.view.sel():
             cursorWord = self.view.substr(region)
@@ -54,13 +69,13 @@ class MoveSelMultiByteWordCommand(sublime_plugin.TextCommand):
 
     def moveCursor(self, text, courseA, defSelWord):
         whileList = []
-        defSetIndex = 0;
+        defSetIndex = 0
         results = wordParse(text)
         length = len(defSelWord)
 
-        if courseA == True:
+        if courseA is True:
             setIndex = 0
-            courseB   = False
+            courseB = False
             while length > 0:
                 if results[setIndex] == defSelWord[defSetIndex]:
                     whileList.append(len(defSelWord[defSetIndex]))
@@ -69,7 +84,7 @@ class MoveSelMultiByteWordCommand(sublime_plugin.TextCommand):
                 length -= 1
         else:
             setIndex = -1
-            courseB   = True
+            courseB = True
             while length > 0:
                 if results[-1] == defSelWord[length-1]:
                     whileList.append(len(defSelWord[length-1]))
@@ -79,6 +94,7 @@ class MoveSelMultiByteWordCommand(sublime_plugin.TextCommand):
         if len(results) <= 0:
             self.view.run_command("move", {"by": "characters", "forward": courseA, "extend": True})
             return True
+
         whileList.append(len(results[setIndex]))
         self.view.run_command("move", {"by": "characters", "forward": courseB})
         for x in whileList:
